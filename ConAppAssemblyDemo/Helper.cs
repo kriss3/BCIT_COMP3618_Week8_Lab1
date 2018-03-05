af58be6a120bdcb1e2922acdfdada02702984978
kszczurowski <kszczurowski@absolute.com>
2018-03-05 00:08:06 -0800

    Week 8 Lab 1 inspecting assembly. Fully working solution.

---
+++ ConAppAssemblyDemo/Helper.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using static System.Console;
+
+namespace ConAppAssemblyDemo
+{
+    public static class Helper
+    {
+        public static void ShowAssemblyInfo(Assembly a)
+        {
+            WriteLine(a.FullName);
+            WriteLine("From GAC? {0}", a.GlobalAssemblyCache);
+            WriteLine("Path: {0}", a.Location);
+            WriteLine("Version: {0}", a.ImageRuntimeVersion);
+            
+            // Show Modules
+            foreach (Module m in a.GetModules())
+            {
+                WriteLine(" Mod: {0}", m.Name);
+            }
+        }
+    }
+}
---
+++ ConAppAssemblyDemo/MyApp.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using static System.Console;
+
+namespace ConAppAssemblyDemo
+{
+    class MyApp
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727\System.dll";
+            // Load a specific Assembly
+            Assembly a = Assembly.LoadFile(path);
+            Helper.ShowAssemblyInfo(a);
+            // Get our Assembly
+            Assembly ourAssembly = Assembly.GetExecutingAssembly();
+            Helper.ShowAssemblyInfo(ourAssembly);
+            Read();
+        }
+    }
+}
